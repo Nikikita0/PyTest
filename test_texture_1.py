902e66a3f0898b8eb8078f313409838dc888dc8a
admin <suharevnikita161201@gmail.com>
2023-10-12 20:26:21 +0300

    third part

--- test_texture_1.py
+++ test_texture_1.py
@@ -25,3 +25,22 @@ class TestPage1():
         self.browser.get(link)
         self.browser.find_element(By.XPATH, "//a[@href = 'cart/show']")
 
+
+class TestPage2():
+
+    def setup_method(self):  # будет влиять на отдельный метод
+        print("\nstart browser for test suite..")
+        self.browser = webdriver.Chrome()
+
+    def teardown_method(self):  # будет влиять на отдельный метод
+        print("\nquit browser for test suite..")
+        self.browser.quit()
+
+    def test_is_login_link(self):
+        self.browser.get(link)
+        self.browser.find_element(By.XPATH, "//div[@class = 'top_bar_user']/a[@href = 'user/login']")
+
+    def test_is_basket_link_on_the_main_page(self):
+        self.browser.get(link)
+        self.browser.find_element(By.XPATH, "//a[@href = 'cart/show']")
+
