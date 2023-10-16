0abb02ed9b16eaef60702747bb7d92277b485ca4
admin <suharevnikita161201@gmail.com>
2023-10-16 19:22:48 +0300

    third part

--- test_texture_3.py
+++ test_texture_3.py
@@ -5,9 +5,9 @@ from selenium.webdriver.common.by import By
 
 link = 'https://casenik.com.ua/'
 
-@pytest.fixture # для каждой функции
 
-#@pytest.fixture(scope="class")  # для целого класса
+@pytest.fixture  # для каждой функции
+# @pytest.fixture(scope="class")  # для целого класса
 def browser():
     print('\nstart browser for test..')
     browser = webdriver.Chrome()
@@ -16,12 +16,14 @@ def browser():
     browser.quit()
 
 # Авто использование текстур
+
+
 @pytest.fixture(autouse=True)  # автоиспользование фикстур
 def prepare():
     print('Печатай каждый тест')
 
 
-class TestPage1():
+class TestPage1:
     def test_is_button_search(self, browser):
         browser.get(link)
         browser.find_element(By.XPATH, "//button[@class= 'header_search_button trans_300']")
